3928ce70119a9bb246981f1b8377ab5664e54194
Paolacocc <coccettipaola@gmail.com>
2023-03-13 15:16:23 +0100

    Mail: javascript done

---
+++ mail/js/script.js
@@ -0,0 +1,29 @@
+//chiedi all'utente la sua mail
+//crea una lista di chi puo accedere
+//controlla che l'email dell'utente sia nella lista che hai creato
+//SE
+// e nella lista scrivi si puoi accedere
+//ALTRIMENTI
+// scrivi scusa non hai accesso 
+
+const userMail = prompt(" qual'e' la tua email?");
+
+const mailList = ['bianchi@mail.com', 'rossi@mail.com', 'verdi@mail.com'];
+console.log(mailList);
+
+let youHaveAccess = false;
+
+for (let i = 0; i < mailList.length; i++){
+    const currentMail = mailList[i];
+    // console.log(currentMail);
+    if (currentMail.toLowerCase() === userMail.toLowerCase()) {
+        youHaveAccess = true;
+    }
+    
+}
+
+if (youHaveAccess === true) {
+    console.log("Yeah!! you have access to this content");
+} else {
+    console.log("Sorry try again");
+}
